4cc55f067aeb86a204c36d4f9725705308435714
dge04200 <dge04200@naver.com>
2024-02-27 10:27:34 +0900

    관리자 추가

--- gagevue/src/main/java/kr/happyjob/study/controller/comncod/ComnCodController.java
+++ gagevue/src/main/java/kr/happyjob/study/controller/comncod/ComnCodController.java
@@ -19,6 +19,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import kr.happyjob.study.vo.comncod.ComnGrpCodModel;
+import kr.happyjob.study.vo.comncod.ComnDtlCodModel;
 import kr.happyjob.study.service.comncod.ComnCodService;
 
 
@@ -74,4 +75,265 @@ public class ComnCodController {
 		return resultMap;
 	}
 	
+	/**
+	 *  공통 그룹 코드 저장
+	 */
+	@RequestMapping("saveComnGrpCod.do")
+	//Map 형태 redirect안할때 씀 즉 값만 바꾸겠다.라는 이야기
+	@ResponseBody
+	public Map<String, Object> saveComnGrpCod(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".saveComnGrpCod");
+		logger.info("   - paramMap : " + paramMap);
+		
+		String action = (String)paramMap.get("action");
+		
+		String result = "SUCCESS";
+		String resultMsg = "저장 되었습니다.";
+		
+		
+		// 사용자 정보 설정
+		paramMap.put("fst_rgst_sst_id", session.getAttribute("loginId"));
+		paramMap.put("fnl_mdfr_sst_id", session.getAttribute("loginId"));
+		if ("I".equals(action)) {
+			// 그룹코드 신규 저장
+			comnCodService.insertComnGrpCod(paramMap);
+		} else if("U".equals(action)) {
+			// 그룹코드 수정 저장
+			comnCodService.updateComnGrpCod(paramMap);
+		} else {
+			result = "FALSE";
+			resultMsg = "알수 없는 요청 입니다.";
+		}
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", result);
+		resultMap.put("resultMsg", resultMsg);
+		
+		logger.info("+ End " + className + ".saveComnGrpCod");
+		
+		return resultMap;
+	}
+	
+	/**
+	 *  공통 그룹 코드 단건 조회
+	 */
+	@RequestMapping("selectComnGrpCod.do")
+	@ResponseBody
+	public Map<String, Object> selectComnGrpCod (Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".selectComnGrpCod");
+		logger.info("   - paramMap : " + paramMap);
+
+		String result = "SUCCESS";
+		String resultMsg = "조회 되었습니다.";
+		
+		// 공통 그룹 코드 단건 조회
+		ComnGrpCodModel comnGrpCodModel = comnCodService.selectComnGrpCod(paramMap);
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", result);
+		resultMap.put("resultMsg", resultMsg);
+		resultMap.put("comnGrpCodModel", comnGrpCodModel);
+		
+		logger.info("+ End " + className + ".selectComnGrpCod");
+		
+		return resultMap;
+	}
+	
+	/**
+	 *  그룹코드 삭제
+	 */
+	@RequestMapping("deleteComnGrpCod.do")
+	@ResponseBody
+	public Map<String, Object> deleteComnGrpCod(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".deleteComnGrpCod");
+		logger.info("   - paramMap : " + paramMap);
+
+		String result = "SUCCESS";
+		String resultMsg = "삭제 되었습니다.";
+		
+		// 그룹코드 삭제
+		comnCodService.deleteComnGrpCod(paramMap);
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", result);
+		resultMap.put("resultMsg", resultMsg);
+		
+		logger.info("+ End " + className + ".deleteComnGrpCod");
+		
+		return resultMap;
+	}
+	
+	/**
+	 * 공통 상세 코드 목록 조회
+	 */
+	@RequestMapping("listComnDtlCod.do")
+	public String listComnDtlCod(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".listComnDtlCod");
+		logger.info("   - paramMap : " + paramMap);
+		
+		
+		int currentPage = Integer.parseInt((String)paramMap.get("currentPage"));	// 현재 페이지 번호
+		int pageSize = Integer.parseInt((String)paramMap.get("pageSize"));			// 페이지 사이즈
+		int pageIndex = (currentPage-1)*pageSize;												// 페이지 시작 row 번호
+		
+
+		paramMap.put("pageIndex", pageIndex);
+		paramMap.put("pageSize", pageSize);
+		
+		// 공통 상세코드 목록 조회
+		List<ComnDtlCodModel> listComnDtlCodModel = comnCodService.listComnDtlCod(paramMap);
+		model.addAttribute("listComnDtlCodModel", listComnDtlCodModel);
+		
+		// 공통 상세코드 목록 카운트 조회
+		int totalCount = comnCodService.countListComnDtlCod(paramMap);
+		model.addAttribute("totalCntComnDtlCod", totalCount);
+		
+		model.addAttribute("pageSize", pageSize);
+		model.addAttribute("currentPageComnDtlCod",currentPage);
+		
+		logger.info("+ End " + className + ".listComnDtlCod");
+
+		return "/system/comnDtlCodList";
+	}
+	
+	/**
+	 *  공통 상세 코드 단건 조회
+	 */
+	@RequestMapping("selectComnDtlCod.do")
+	@ResponseBody
+	public Map<String, Object> selectComnDtlCod (Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".selectComnDtlCod");
+		logger.info("   - paramMap : " + paramMap);
+
+		String result = "SUCCESS";
+		String resultMsg = "조회 되었습니다.";
+		
+		// 공통 상세 코드 단건 조회
+		ComnDtlCodModel comnDtlCodModel = comnCodService.selectComnDtlCod(paramMap);
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", result);
+		resultMap.put("resultMsg", resultMsg);
+		resultMap.put("comnDtlCodModel", comnDtlCodModel);
+		
+		logger.info("+ End " + className + ".selectComnDtlCod");
+		
+		return resultMap;
+	}
+	
+	/**
+	 *  공통 상세 코드 저장
+	 */
+	@RequestMapping("saveComnDtlCod.do")
+	@ResponseBody
+	public Map<String, Object> saveComnDtlCod(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".saveComnDtlCod");
+		logger.info("   - paramMap : " + paramMap);
+
+		String action = (String)paramMap.get("action");
+		String result = "SUCCESS";
+		String resultMsg = "저장 되었습니다.";
+		
+		// 사용자 정보 설정
+		paramMap.put("fst_rgst_sst_id", session.getAttribute("loginId"));
+		paramMap.put("fnl_mdfr_sst_id", session.getAttribute("loginId"));
+		
+		if ("I".equals(action)) {
+			// 상세코드 신규 저장
+			comnCodService.insertComnDtlCod(paramMap);
+		} else if("U".equals(action)) {
+			// 상세코드 수정 저장
+			comnCodService.updateComnDtlCod(paramMap);
+		} else {
+			result = "FALSE";
+			resultMsg = "알수 없는 요청 입니다.";
+		}
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", result);
+		resultMap.put("resultMsg", resultMsg);
+		
+		logger.info("+ End " + className + ".saveComnDtlCod");
+		
+		return resultMap;
+	}
+	
+	/**
+	 * 공통 상세 코드 목록 조회
+	 */
+	@RequestMapping("comcombo.do")
+	@ResponseBody
+	public Map<String, Object> comcombo(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".comcombo");
+		logger.info("   - paramMap : " + paramMap);
+		
+		
+		int currentPage = 1;	// 현재 페이지 번호
+		int pageSize = 999;			// 페이지 사이즈
+		int pageIndex = 0;												// 페이지 시작 row 번호
+		
+
+		paramMap.put("pageIndex", pageIndex);
+		paramMap.put("pageSize", pageSize);
+		
+		// 공통 상세코드 목록 조회
+		List<ComnDtlCodModel> listComnDtlCodModel = comnCodService.listComnDtlCod(paramMap);
+		model.addAttribute("listComnDtlCodModel", listComnDtlCodModel);
+		
+		// 공통 상세코드 목록 카운트 조회
+		int totalCount = comnCodService.countListComnDtlCod(paramMap);
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		
+		resultMap.put("listComnDtlCodModel", listComnDtlCodModel);
+		
+		resultMap.put("totalCntComnDtlCod", totalCount);
+		
+		resultMap.put("pageSize", pageSize);
+		resultMap.put("currentPageComnDtlCod",currentPage);
+		
+		logger.info("+ End " + className + ".listComnDtlCod");
+
+		return resultMap;
+	}	
+	
+	/**
+	 *  상세코드 삭제
+	 */
+	@RequestMapping("deleteComnDtlCod.do")
+	@ResponseBody
+	public Map<String, Object> deleteComnDtlCod(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) throws Exception {
+		
+		logger.info("+ Start " + className + ".deleteComnDtlCod");
+		logger.info("   - paramMap : " + paramMap);
+
+		String result = "SUCCESS";
+		String resultMsg = "삭제 되었습니다.";
+		
+		// 상세코드 삭제
+		comnCodService.deleteComnDtlCod(paramMap);
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", result);
+		resultMap.put("resultMsg", resultMsg);
+		
+		logger.info("+ End " + className + ".deleteComnDtlCod");
+		
+		return resultMap;
+	}
 }
